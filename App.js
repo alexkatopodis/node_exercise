d1fea158d60087e138d31da1efee8a5f6b0c6975
Alex Pereira Katopodis <aperei83@its.jnj.com>
2023-10-20 16:47:53 -0300

    add cors config

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // Imports
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./src/routes/routes');
@@ -20,6 +21,8 @@ app.use(morgan('dev', {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use((req, res, next) => {
 	// Set response headers
